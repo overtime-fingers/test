8f5b5e778ae16f53f3c4805df3ee9b8ed78bfbfa
overtime-fingers <mono4682@gmail.com>
2020-06-02 01:01:35 +0900

    수정적용

--- greenic/web/js/script.js
+++ greenic/web/js/script.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
-    $(window).scroll(function(){
-        var height = $(document).scrollTop();
-        if(height > 0){ 
-            $('.header').addClass('on');
-        }else if(height == 0){ 
-            $('.header').removeClass('on');
-        } 
-    })
+    // $(window).scroll(function(){
+    //     var height = $(document).scrollTop();
+    //     if(height > 0){ 
+    //         $('.header').addClass('on');
+    //     }else if(height == 0){ 
+    //         $('.header').removeClass('on');
+    //     } 
+    // });
+
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 4,
+        spaceBetween: 28,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+      });
 });
